9432738db2070b06802abaec6b43e8adb949c501
bestauffer <77029920+bestauffer@users.noreply.github.com>
2023-06-29 05:51:23 -0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,10 +3,7 @@ import { Application, Router } from "https://deno.land/x/oak@v12.5.0/mod.ts";
 import { CorsOptions, oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import "https://deno.land/x/dotenv@v3.2.2/load.ts";
 //import { SMTPClient } from "https://deno.land/x/denomailer@1.6.0/mod.ts";
-import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
-const client = new SmtpClient({
-  content_encoding: "quoted-printable", // 7bit, 8bit, base64, binary, quoted-printable
-});
+
 const { EMAIL, PORT, PWD, RECEIVING_EMAIL } = Deno.env.toObject();
 // const client = new SMTPClient({
 //   connection: {
@@ -30,41 +27,6 @@ const loadOriginsFromDataBase = async () => {
   await sleep(500);
   return ["https://bestauffer.github.io", "http://localhost:3000"];
 };
-
-const myPoster = async () => {
-  //console.log("hi");
-  try{
-    //await client.connectTLS(connectConfig);
-    //console.log(EMAIL);
-    console.log("hi");
-     await client.connectTLS({
-      hostname: "smtp.gmail.com",
-      port: 465,
-      username: EMAIL,
-      password: PWD,
-    });
-    // await client.connectTLS({
-    //   // hostname: "smtp.gmail.com",
-    //   // port: 465,
-    //   username: EMAIL,
-    //   password: PWD,
-    // });
-
-    console.log("hi");
-    await client.send({
-      from: EMAIL,
-      to: RECEIVING_EMAIL,
-      subject: "Welcome!",
-      content: "Hi from Vuelancer!",
-    });
-    
-    await client.close();
-  }
-  catch{
-    console.log("failed");
-  }
- 
-}
 
 const books = new Map<string, any>();
 books.set("1", {

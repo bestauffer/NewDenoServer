2a19094b4c408016a2241f1d9751bae474a96cc3
bestauffer <77029920+bestauffer@users.noreply.github.com>
2023-06-29 03:08:34 -0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,10 +1,23 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.5.0/mod.ts";
 import { CorsOptions, oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import "https://deno.land/x/dotenv@v3.2.2/load.ts";
-import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
+//import { SMTPClient } from "https://deno.land/x/denomailer@1.6.0/mod.ts";
 import "https://deno.land/x/dotenv@v3.2.2/load.ts";
+import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
 const client = new SmtpClient();
 const { EMAIL, PORT, EMAIL_PASSWORD, RECEIVING_EMAIL } = Deno.env.toObject();
+// const client = new SMTPClient({
+//   connection: {
+//     hostname: "smtp.aol.com",
+//     port: 465,
+//     // 
+//     auth: {
+//       username: EMAIL,
+//       password: EMAIL_PASSWORD,
+//     },
+//     tls: true,
+//   },
+// });
 
 const sleep = (ms: number) =>
   new Promise((resolve) => {
@@ -12,22 +25,31 @@ const sleep = (ms: number) =>
   });
 
 const loadOriginsFromDataBase = async () => {
-  await sleep(3000);
+  await sleep(500);
   return ["https://bestauffer.github.io", "http://localhost:3000"];
 };
 
 const myPoster = async () => {
+  //console.log("hi");
   try{
-    await client.connectTLS(connectConfig);
+    //await client.connectTLS(connectConfig);
     //console.log(EMAIL);
-      await client.send({
-        from: EMAIL,
-        to: RECEIVING_EMAIL,
-        subject: "Welcome!",
-        content: "Hi from Test!",
-      });
+    const connectConfig: any = {
+      hostname: "smtp.gmail.com",
+      port: 465,
+      username: EMAIL,
+      password: EMAIL_PASSWORD,
+    };
+    await client.connectTLS(connectConfig);
+    console.log("hi");
+    await client.send({
+      from: EMAIL,
+      to: RECEIVING_EMAIL,
+      subject: "Welcome!",
+      content: "Hi from Vuelancer!",
+    });
     
-      await client.close();
+    await client.close();
   }
   catch{}
  
@@ -40,13 +62,13 @@ books.set("1", {
   author: "Mary Shelley",
 });
 
-const connectConfig: any = {
-  hostname: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  username: EMAIL,
-  password: EMAIL_PASSWORD,
-};
+// const connectConfig: any = {
+//   hostname: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   username: EMAIL,
+//   password: EMAIL_PASSWORD,
+// };
 
 const corsOptions: CorsOptions = {
   origin: async () => {
@@ -59,13 +81,14 @@ const corsOptions: CorsOptions = {
 const PORTO: number = parseInt(PORT);
 
 const router = new Router();
-router.get("/book", oakCors(), (context) => {
+router.get("/book", oakCors(corsOptions), (context) => {
   context.response.body = Array.from(books.values());
 });
-router.get("/", oakCors(), (context) => {
+router.get("/", oakCors(corsOptions), (context) => {
+
   context.response.body = { message: "This message is coming from a get call on deno server" };
 });
-router.get('/email', oakCors(), (context) => {
+router.put('/email', oakCors(corsOptions), (context) => {
   myPoster();
   context.response.body = { message: "This message is coming from a post call on a deno server" };
 
